c4675cfcc0234fece94604d2aea76fbaeaad4893
Anish Maharjan <maharjan.rule@gmail.com>
2025-01-19 09:17:52 +0545

    improvement and fixes

--- app/src/main/java/com/xdroid/app/changewallpaper/data/UrlName.kt
+++ app/src/main/java/com/xdroid/app/changewallpaper/data/UrlName.kt
@@ -4,7 +4,7 @@ import com.xdroid.app.changewallpaper.BuildConfig
 
 object UrlName {
     val allImageList = "pictures/records?filter=(category=%27${BuildConfig.FLAVOR}%27)"
-    val allImages = "pictures/records"
+    val allImages = "pictures/records?sort=-created"
     val animeImages = "pictures/records?filter=(category=%27anime%27)"
     val imageUrl = "https://anish.pockethost.io/api/files/"
     val adUrl = "ad/records?filter=(type=%27centerBanner%27)"
